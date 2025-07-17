6b060328cc5860e370e280c85985a2f660e2e09a
Rakhmanberdi <rakhmanberdi.izbassar@narxoz.kz>
2025-07-17 16:52:02 +0500

    Add requirements.txt

--- aventra/settings.py
+++ aventra/settings.py
@@ -142,12 +142,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
 
 
 MEDIA_URL = '/media/'
